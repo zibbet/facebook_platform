5bec0fc0bb82c3cfb7b329d4d2a00d51007c0449
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2019-09-12 15:39:40 +1000

    init commit

---
+++ lib/facebook_platform.rb
@@ -0,0 +1,6 @@
+require 'facebook_platform/version'
+
+module FacebookPlatform
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/facebook_platform_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe FacebookPlatform do
+  it 'has a version number' do
+    expect(FacebookPlatform::VERSION).not_to be nil
+  end
+end
