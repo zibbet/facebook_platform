e807a37f83d51079b76290d1b79083aba8b4edbb
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2019-10-16 14:58:23 +1000

    add business page find method to get page details

--- lib/facebook_platform/businesses/page.rb
+++ lib/facebook_platform/businesses/page.rb
@@ -11,9 +11,15 @@ module FacebookPlatform
         result['data'].map { |hash| new(id: hash['id'], name: hash['name']) }
       end
 
-      def initialize(id:, name:)
+      def self.find(page_id:, access_token:)
+        result = API.get(page_id, access_token: access_token)
+        new(id: result['id'], name: result['name'], access_token: result['access_token'])
+      end
+
+      def initialize(id:, name:, access_token: '')
         @id = id
         @name = name
+        @access_token = access_token
       end
     end
   end
