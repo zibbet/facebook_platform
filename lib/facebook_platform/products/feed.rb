44e6c066e96a4fe64d828fe1b3db6596a773ebef
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2019-11-27 10:47:47 +1000

    add feed create/update new default params, add options

--- lib/facebook_platform/products/feed.rb
+++ lib/facebook_platform/products/feed.rb
@@ -3,26 +3,27 @@
 module FacebookPlatform
   module Products
     # represents Facebook's Product Feed entity
+    # https://developers.facebook.com/docs/marketing-api/reference/product-feed-schedule/
     class Feed
       attr_reader :id, :name
 
       # rubocop:disable  Metrics/ParameterLists
-      def self.create(access_token:, catalog_id:, name:, products_csv_url:, interval: 'DAILY', hour: 23)
+      def self.create(access_token:, catalog_id:, name:, products_csv_url:, interval: 'HOURLY', options: {})
         result = API.post(
           "#{catalog_id}/product_feeds",
           access_token: access_token,
           name: name,
-          schedule: { interval: interval, url: products_csv_url, hour: hour }
+          schedule: { interval: interval, url: products_csv_url }.merge(options)
         )
         new(id: result['id'])
       end
       # rubocop:enable  Metrics/ParameterLists
 
-      def self.update(access_token:, product_feed_id:, products_csv_url:, interval: 'DAILY', hour: 23)
+      def self.update(access_token:, product_feed_id:, products_csv_url:, interval: 'HOURLY', options: {})
         result = API.post(
           product_feed_id,
           access_token: access_token,
-          update_schedule: { interval: interval, url: products_csv_url, hour: hour }
+          update_schedule: { interval: interval, url: products_csv_url }.merge(options)
         )
         result['success']
       end
--- spec/facebook_platform/products/feed_spec.rb
+++ spec/facebook_platform/products/feed_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe FacebookPlatform::Products::Feed do
   end
 
   context '.create' do
-    it 'returns a created object serialized from hash' do
+    it 'returns a created object serialized from hash with custom options' do
       expect(FacebookPlatform::API).to receive(:post).with(
         '999022/product_feeds',
         access_token: 'ABC-123',
@@ -27,6 +27,27 @@ RSpec.describe FacebookPlatform::Products::Feed do
         access_token: 'ABC-123',
         catalog_id: '999022',
         name: 'Death Star',
+        products_csv_url: 'https://www.zibbet.com/path/to/upload_products.csv',
+        interval: 'DAILY',
+        options: { hour: 23 }
+      )
+      expect(result).to have_attributes(id: '100041623866064')
+    end
+
+    it 'returns a created object serialized from hash with default options' do
+      expect(FacebookPlatform::API).to receive(:post).with(
+        '999022/product_feeds',
+        access_token: 'ABC-123',
+        name: 'Death Star',
+        schedule: {
+          interval: 'HOURLY',
+          url: 'https://www.zibbet.com/path/to/upload_products.csv'
+        }
+      ).and_return('id' => '100041623866064')
+      result = described_class.create(
+        access_token: 'ABC-123',
+        catalog_id: '999022',
+        name: 'Death Star',
         products_csv_url: 'https://www.zibbet.com/path/to/upload_products.csv'
       )
       expect(result).to have_attributes(id: '100041623866064')
@@ -34,7 +55,7 @@ RSpec.describe FacebookPlatform::Products::Feed do
   end
 
   context '.update' do
-    it 'returns true on success' do
+    it 'returns true on success with custom options' do
       expect(FacebookPlatform::API).to receive(:post).with(
         '999022',
         access_token: 'ABC-123',
@@ -42,6 +63,25 @@ RSpec.describe FacebookPlatform::Products::Feed do
           interval: 'DAILY',
           url: 'https://www.zibbet.com/path/to/upload_products.csv',
           hour: 23
+        }
+      ).and_return('success' => true)
+      result = described_class.update(
+        access_token: 'ABC-123',
+        product_feed_id: '999022',
+        products_csv_url: 'https://www.zibbet.com/path/to/upload_products.csv',
+        interval: 'DAILY',
+        options: { hour: 23 }
+      )
+      expect(result).to be_truthy
+    end
+
+    it 'returns true on success with default options' do
+      expect(FacebookPlatform::API).to receive(:post).with(
+        '999022',
+        access_token: 'ABC-123',
+        update_schedule: {
+          interval: 'HOURLY',
+          url: 'https://www.zibbet.com/path/to/upload_products.csv'
         }
       ).and_return('success' => true)
       result = described_class.update(
