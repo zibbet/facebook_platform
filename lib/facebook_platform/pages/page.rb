86c5f9f968f2e2f12dc8b12edd372ca53aeebffb
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2020-01-16 11:05:32 +1000

    add a link to default FB page attributes

--- lib/facebook_platform/pages/page.rb
+++ lib/facebook_platform/pages/page.rb
@@ -4,22 +4,23 @@ module FacebookPlatform
   module Pages
     # represents Facebook's Page entity
     class Page
-      attr_reader :id, :name, :access_token
+      attr_reader :id, :name, :access_token, :link
 
       def self.all(access_token:)
         result = API.get('me/accounts', access_token: access_token)
         result['data'].map { |hash| new(id: hash['id'], name: hash['name'], access_token: hash['access_token']) }
       end
 
-      def self.find(page_id:, access_token:)
-        result = API.get(page_id, access_token: access_token, fields: 'access_token,name')
-        new(id: result['id'], name: result['name'], access_token: result['access_token'])
+      def self.find(page_id:, access_token:, fields: 'access_token,name,link')
+        result = API.get(page_id, access_token: access_token, fields: fields)
+        new(id: result['id'], name: result['name'], access_token: result['access_token'], link: result['link'])
       end
 
-      def initialize(id:, name:, access_token: '')
+      def initialize(id:, name:, access_token: '', link: '')
         @id = id
         @name = name
         @access_token = access_token
+        @link = link
       end
     end
   end
--- spec/facebook_platform/pages/page_spec.rb
+++ spec/facebook_platform/pages/page_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe FacebookPlatform::Pages::Page do
       expect(page.id).to eq(123)
       expect(page.name).to eq('Death Star')
       expect(page.access_token).to eq('')
+      expect(page.link).to eq('')
     end
   end
 
@@ -26,10 +27,20 @@ RSpec.describe FacebookPlatform::Pages::Page do
   context '.find' do
     it 'returns an objects created from a request result hash' do
       expect(FacebookPlatform::API).to receive(:get).with(
-        1_234_567, access_token: 'ABC-123', fields: 'access_token,name'
-      ).and_return('id' => '1234567', 'name' => 'Death Star', 'access_token' => 'ABC-123')
+        1_234_567, access_token: 'ABC-123', fields: 'access_token,name,link'
+      ).and_return(
+        'id' => '1234567',
+        'name' => 'Death Star',
+        'access_token' => 'ABC-123',
+        'link' => 'https://facebook.com/Death-Star-1234567'
+      )
       result = described_class.find(page_id: 1_234_567, access_token: 'ABC-123')
-      expect(result).to have_attributes(id: '1234567', name: 'Death Star', access_token: 'ABC-123')
+      expect(result).to have_attributes(
+        id: '1234567',
+        name: 'Death Star',
+        access_token: 'ABC-123',
+        link: 'https://facebook.com/Death-Star-1234567'
+      )
     end
   end
 end
