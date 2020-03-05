9fd7bfea656bfbd8a759edbbaa8e5819f706145c
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2020-03-05 14:13:09 +1000

    add Product.exists_in_catalog? method

--- lib/facebook_platform/products/product.rb
+++ lib/facebook_platform/products/product.rb
@@ -2,8 +2,19 @@
 
 module FacebookPlatform
   module Products
-    # represents Facebook's Catalog entity
+    # represents Facebook's product catalog entity
     class Product
+      # https://developers.facebook.com/docs/marketing-api/reference/product-catalog/products/
+      def self.exists_in_catalog?(access_token:, catalog_id:, retailer_id:)
+        result = API.get(
+          "#{catalog_id}/products",
+          access_token: access_token,
+          filter: { retailer_id: { eq: retailer_id } },
+          summary: true
+        )
+        result.dig('summary', 'total_count') == 1
+      end
+
       # https://developers.facebook.com/docs/commerce-platform/catalog/batch-api
       def self.delete_all(access_token:, catalog_id:, retailer_ids:)
         requests = retailer_ids.map do |retailer_id|
--- spec/facebook_platform/products/product_spec.rb
+++ spec/facebook_platform/products/product_spec.rb
@@ -4,6 +4,30 @@ require 'facebook_platform/api'
 require 'facebook_platform/products/product'
 
 RSpec.describe FacebookPlatform::Products::Product do
+  context '.exists_in_catalog?' do
+    it 'returns true if a product was found by retailed_id in a catalog' do
+      expect(FacebookPlatform::API).to receive(:get).with(
+        '12345/products',
+        access_token: 'ABC-123',
+        filter: { retailer_id: { eq: 432 } },
+        summary: true
+      ).and_return('summary' => { 'total_count' => 1 })
+      result = described_class.exists_in_catalog?(access_token: 'ABC-123', catalog_id: '12345', retailer_id: 432)
+      expect(result).to be_truthy
+    end
+
+    it 'returns false if a product was not found by retailed_id in a catalog' do
+      expect(FacebookPlatform::API).to receive(:get).with(
+        '12345/products',
+        access_token: 'ABC-123',
+        filter: { retailer_id: { eq: 432 } },
+        summary: true
+      ).and_return('summary' => { 'total_count' => 0 })
+      result = described_class.exists_in_catalog?(access_token: 'ABC-123', catalog_id: '12345', retailer_id: 432)
+      expect(result).to be_falsey
+    end
+  end
+
   context '.delete_all' do
     it 'returns a hash on success' do
       expect(FacebookPlatform::API).to receive(:post).with(
